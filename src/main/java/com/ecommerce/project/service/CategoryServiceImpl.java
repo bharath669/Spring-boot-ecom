4c418121250bf006ceb95165ed5b3c30ca6c1afe
“bharath669” <vvbharath2006@gmail.com>
2025-09-22 19:09:15 +0100

    deleting the par Id With handling id exist or not

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -26,7 +26,10 @@ public class CategoryServiceImpl implements CategoryService{
     public String deleteCategory(Long categoryId) {
         Category category=categories.stream()
                 .filter(c ->c.getCategoryId().equals(categoryId))
-                .findFirst().get();
+                .findFirst().orElse(null);
+        if(category==null){
+            return "Category is null";
+        }
         categories.remove(category);
 
         return "Category with CategoryId"+categoryId+"deleted successfully";
