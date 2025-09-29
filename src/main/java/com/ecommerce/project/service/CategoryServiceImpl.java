2c3fbf7e9405d2a91f2b5eb3197b03c5d53e3617
“bharath669” <vvbharath2006@gmail.com>
2025-09-29 11:05:17 +0100

    Validation for No Categories present

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -5,6 +5,7 @@ import com.ecommerce.project.exceptions.ResourceNotFoundException;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,7 +19,11 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
+        List<Category> categories=categoryRepository.findAll();
+        if(categories.isEmpty()){
+            throw new APIException("No Category created till now ");
+        }
+        return categories;
     }
 
     @Override
