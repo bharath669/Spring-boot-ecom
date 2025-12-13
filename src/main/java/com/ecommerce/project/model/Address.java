2ed2f602fd392577097bf4f457dc9c92e8bdc1c0
“bharath669” <vvbharath2006@gmail.com>
2025-12-13 13:08:37 +0530

    Creating Address

--- src/main/java/com/ecommerce/project/model/Address.java
+++ src/main/java/com/ecommerce/project/model/Address.java
@@ -6,10 +6,6 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name = "address")
@@ -22,39 +18,39 @@ public class Address {
     private Long addressId;
 
     @NotBlank
-    @Size(min = 5,message = "street name must be atleast 5 Characters ")
-    private String street;
-
-    @NotBlank
-    @Size(min = 5,message = "building name must be atleast 5 Characters ")
-    private String buildingName;
+    @Size(min = 3,message = "country name must be atleast 3 Characters ")
+    private String country;
 
     @NotBlank
     @Size(min = 4,message = "city name must be atleast 4 Characters ")
     private String city;
 
     @NotBlank
+    @Size(min = 5,message = "street name must be atleast 5 Characters ")
+    private String street;
+
+    @NotBlank
+    @Size(min = 5,message = "pinCode must be atleast 5 Characters ")
+    private String pin_code;
+
+    @NotBlank
+    @Size(min = 5,message = "building name must be atleast 5 Characters ")
+    private String buildingName;
+
+    @NotBlank
     @Size(min = 4,message = "state name must be atleast 4 Characters ")
     private String state;
 
-    @NotBlank
-    @Size(min = 3,message = "country name must be atleast 3 Characters ")
-    private String country;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
-    @NotBlank
-    @Size(min = 6,message = "pinCode must be atleast 6 Characters ")
-    private String pinCode;
-
-    @ToString.Exclude
-    @ManyToMany(mappedBy = "addresses")
-    private List<User> users=new ArrayList<>();
-
-    public Address(String street, String buildingName, String city, String state, String country, String pinCode) {
+    public Address(String street, String buildingName, String city, String state, String country, String pincode) {
         this.street = street;
         this.buildingName = buildingName;
         this.city = city;
         this.state = state;
         this.country = country;
-        this.pinCode = pinCode;
+        this.pin_code = pincode;
     }
 }
--- src/main/java/com/ecommerce/project/security/webSecurityConfig.java
+++ src/main/java/com/ecommerce/project/security/webSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -76,6 +77,7 @@ public class webSecurityConfig {
                         .requestMatchers("/api/admin/**").permitAll()
                         .requestMatchers("/api/test/**").permitAll()
                         .requestMatchers("/images/**").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().authenticated());
         http.authenticationProvider(daoAuthenticationProvider());
         http.addFilterBefore(authenticationJwtTokenFilter(),
